6cc180f2a5cd878a70d4f6cd3bc4d41f88c20bff
Gaël La Rochelle <gael_coupe17@outlook.fr>
2023-10-12 10:32:25 +0200

    aout des migrations v2

--- stock_overflow/migrations/Version20231010073505.php
+++ stock_overflow/migrations/Version20231010073505.php
@@ -21,7 +21,7 @@ final class Version20231010073505 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE shipping_shipping_item (shipping_id INT NOT NULL, shipping_item_id INT NOT NULL, INDEX IDX_7851C6DE4887F3F8 (shipping_id), INDEX IDX_7851C6DEF427549A (shipping_item_id), PRIMARY KEY(shipping_id, shipping_item_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        // $this->addSql('CREATE TABLE shipping_shipping_item (shipping_id INT NOT NULL, shipping_item_id INT NOT NULL, INDEX IDX_7851C6DE4887F3F8 (shipping_id), INDEX IDX_7851C6DEF427549A (shipping_item_id), PRIMARY KEY(shipping_id, shipping_item_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE shipping_shipping_item ADD CONSTRAINT FK_7851C6DE4887F3F8 FOREIGN KEY (shipping_id) REFERENCES shipping (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE shipping_shipping_item ADD CONSTRAINT FK_7851C6DEF427549A FOREIGN KEY (shipping_item_id) REFERENCES shipping_item (id) ON DELETE CASCADE');
