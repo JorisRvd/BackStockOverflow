e5e2f939ab814589cd8cb552d49aeeafaed6530b
Gaël La Rochelle <gael_coupe17@outlook.fr>
2023-10-13 15:51:39 +0200

    Fix enum

--- stock_overflow/src/Enums/OrderStatus.php
+++ stock_overflow/src/Enums/OrderStatus.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 
 enum OrderStatus: string {
     
-    case En_attente = 'En attente';
+    case En_attente = 'en attente';
     case Validé = 'validé';
     case Annulé = 'annulé';
 
