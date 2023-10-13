4ca6750751ee42fa9202e229ad404b8b411595c6
Gaël La Rochelle <gael_coupe17@outlook.fr>
2023-10-13 11:12:42 +0200

    Ajout push joris + test cors

--- stock_overflow/config/bundles.php
+++ stock_overflow/config/bundles.php
@@ -10,5 +10,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
